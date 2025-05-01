11cc43535a7f9ef235b540b48e7b39b285b2f901
jhg <1288711+jhg@users.noreply.github.com>
2025-05-01 15:32:08 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Lenorix\LaravelAiJobs\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Lenorix\LaravelAiJobs\AiJobServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
